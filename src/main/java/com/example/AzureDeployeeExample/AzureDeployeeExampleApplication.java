706b813713ce4b824ceb43e0285c73b64c3b8c7e
unknown <nikitamane2000@gmail.com>
2025-05-05 18:48:01 +0530

    first commit

---
+++ src/main/java/com/example/AzureDeployeeExample/AzureDeployeeExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.AzureDeployeeExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureDeployeeExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureDeployeeExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AzureDeployeeExample/AzureDeployeeExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AzureDeployeeExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureDeployeeExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
